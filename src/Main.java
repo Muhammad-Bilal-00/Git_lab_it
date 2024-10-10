d4d5d7dd85b16584de3fb4c742afd2fd0f38ca07
Muhammad-Bilal-00 <151865747+Muhammad-Bilal-00@users.noreply.github.com>
2024-10-10 10:04:29 +0500

    Add simple functionality  of the addition and Multipication

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+class Calculator {
+    private int n1;
+    private int n2;
+    int result;
+
+    int add(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    int mult(int n1, int n2) {
+        return n1 * n2;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Calculator cal = new Calculator();
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Enter your first number:");
+        int n1 = input.nextInt();
+
+
+        System.out.println("Enter your second number:");
+        int n2 = input.nextInt();
+        int additionResult = cal.add(n1, n2);
+        int multiplicationResult = cal.mult(n1, n2);
+        System.out.println("Addition Result: " + additionResult);
+        System.out.println("Multiplication Result: " + multiplicationResult);
+        input.close();
     }
 }
